056fb79885a7e07621b25007cecbf6ff3645948a
jimin <8841258@naver.com>
2021-08-02 21:10:41 +0900

    21:10 jimin

--- mm/src/main/java/co/pooh/myHomePage/command/FromBoardListd.java
+++ mm/src/main/java/co/pooh/myHomePage/command/FromBoardListd.java
@@ -5,17 +5,25 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import co.pooh.myHomePage.board.service.FromBoardService;
 import co.pooh.myHomePage.board.serviceImpl.FromBoardServiceImpl;
 import co.pooh.myHomePage.board.vo.FromBoardVO;
 import co.pooh.myHomePage.common.Command;
+import co.pooh.myHomePage.fmember.vo.FMemberVO;
 
 public class FromBoardListd implements Command {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
 		FromBoardService dao = new FromBoardServiceImpl();
+		
+		//세션 보내기
+		FMemberVO vo = new FMemberVO();
+		HttpSession session = request.getSession();	
+		vo.setNickname((String) session.getAttribute("nickname"));
+		request.setAttribute("member", vo);
 		
 		int n = (int) request.getAttribute("fromno");
 		List<FromBoardVO> list = new ArrayList<FromBoardVO>();
--- mm/src/main/java/co/pooh/myHomePage/command/ToBoardListi.java
+++ mm/src/main/java/co/pooh/myHomePage/command/ToBoardListi.java
@@ -5,17 +5,25 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import co.pooh.myHomePage.board.service.ToBoardService;
 import co.pooh.myHomePage.board.serviceImpl.ToBoardServiceImpl;
 import co.pooh.myHomePage.board.vo.ToBoardVO;
 import co.pooh.myHomePage.common.Command;
+import co.pooh.myHomePage.fmember.vo.FMemberVO;
 
 public class ToBoardListi implements Command {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
 		ToBoardService dao = new ToBoardServiceImpl();
+		
+		//세션 보내기
+		FMemberVO vo = new FMemberVO();
+		HttpSession session = request.getSession();	
+		vo.setAuthor((String) session.getAttribute("author"));
+		request.setAttribute("member", vo);
 		
 		int n = (int) request.getAttribute("tono");
 		List<ToBoardVO> list = new ArrayList<ToBoardVO>();
